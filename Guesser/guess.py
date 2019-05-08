ec438c99f29398c0622751991b0cfea4327a64ad
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-08 20:48:30 +0300

    complete

--- Guesser/guess.py
+++ Guesser/guess.py
@@ -1,4 +1,10 @@
 import random
 
 x = random.randrange(1,100)
-user = int(raw_input('please enter any number.'))
+user = int(raw_input('please enter any number.')),
+if user == x:
+    print('Jackpot'),
+elif x<user:
+    print('Number too low')
+else:
+    print('Your guess is too high')
