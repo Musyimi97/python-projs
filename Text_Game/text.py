c8ac1643dd01052b5776213e1a2756510745b6f7
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-09 21:57:11 +0300

    continue

--- Text_Game/text.py
+++ Text_Game/text.py
@@ -1,8 +1,5 @@
-i = 0
-while i < 7:
+for i in (0,1,2,3,4,5,6,7,8,9):
+    if i==2 or i==4:
+        continue
     print(i)
-    if i == 4:
-        print("Breaking from loop")
-        break
-i += 1
 
