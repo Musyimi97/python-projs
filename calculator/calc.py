53b7e952d4bc006edd64f9526806ad1a5d3afcda
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-11 12:28:31 +0300

    created a small gratitude

--- calculator/calc.py
+++ calculator/calc.py
@@ -2,4 +2,21 @@ from tkinter import*
 
 def calc(source, side):
     storeObj =Frame (source, borderwidth = 1 ,bd = 4,bg ='powder_blue')
-    pass
+    storeObj.pack(side=side, expand=YES , fill= BOTH)
+    return storeObj
+
+def button  (source,side,text, command =None):
+    storeObj = Button(source, text=text, command= command)
+    storeObj.pack(side=side, expand=YES, fill=BOTH)
+    return storeObj
+
+
+class app(Frame):
+    def __init__(self,):
+        Frame.__init__(self)
+        self.option_add('*Font','arial 20 bold')
+        self.pack(expand=YES, fill=BOTH)
+        self.master.title('Calculator')
+
+if __name__ == '__main__':
+    app().mainloop()
