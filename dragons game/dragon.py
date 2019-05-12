128e8846b91cf66b93be228f35bc04ed6c4aa65c
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-12 10:43:09 +0300

    dragon coming through

---
+++ dragons game/dragon.py
@@ -0,0 +1,9 @@
+import random 
+import time
+
+def displayIntro():
+    print('You are in a land full of dragons. ')
+    time.sleep(2)
+    print('''Infront of you ,You see two caves.
+    In one cave, the dragon is friendly and it will share its treasure with you.
+    On the other hand the other dragon is greedy and will eat you on sight''','\n')
